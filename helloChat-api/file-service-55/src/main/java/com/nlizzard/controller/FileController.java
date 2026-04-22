90a95b174b2c97f097ca77bb0890c4b619a1ffac
Blizzard <1315152965@qq.com>
2026-04-22 22:01:38 +0800

    feat: 引入minIO对象存储替换本地文件存储

--- helloChat-api/file-service-55/src/main/java/com/nlizzard/controller/FileController.java
+++ helloChat-api/file-service-55/src/main/java/com/nlizzard/controller/FileController.java
@@ -1,8 +1,14 @@
 package com.nlizzard.controller;
 
+import com.nlizzard.config.MinIOConfig;
 import com.nlizzard.grace.result.GraceJSONResult;
+import com.nlizzard.grace.result.ResponseStatusEnum;
+import com.nlizzard.utils.MinIOUtils;
 import io.micrometer.common.util.StringUtils;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +18,18 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 
+import static com.nlizzard.grace.result.ResponseStatusEnum.FILE_UPLOAD_FAILD;
+
 @RestController
 @RequestMapping("file")
 @Validated
+@RequiredArgsConstructor
 public class FileController {
 
-    // springboot文件上传实现方案
-    @PostMapping("uploadFace")
+    private final MinIOConfig minIOConfig;
+
+    // springboot文件上传实现方案一（传统单体项目可使用）
+    @PostMapping("uploadFace1")
     public GraceJSONResult uploadFace1(@RequestParam("file") MultipartFile file,
                                        @NotNull(message = "用户ID不能为空") String userId) throws Exception {
 
@@ -49,5 +60,30 @@ public class FileController {
         file.transferTo(newFile);
 
         return GraceJSONResult.ok();
+    }
+
+
+    // 分布式存储技术方案minIO实现文件上传（微服务项目推荐使用）
+    @PostMapping("uploadFace")
+    public GraceJSONResult uploadFace(@RequestParam("file") MultipartFile file,
+                                      @NotNull(message = "用户ID不能为空") String userId) throws Exception {
+
+        String filename = file.getOriginalFilename();   // 获得文件原始名称
+        if (StringUtils.isBlank(filename)) {
+            return GraceJSONResult.errorCustom(FILE_UPLOAD_FAILD);
+        }
+        // 定义存放路径，face/userId/filename
+        filename = "face" + "/" + userId + "/" + filename;
+        MinIOUtils.uploadFile(minIOConfig.getBucketName(),
+                filename,
+                file.getInputStream());
+
+        String faceUrl = minIOConfig.getFileHost()
+                + "/"
+                + minIOConfig.getBucketName()
+                + "/"
+                + filename;
+
+        return GraceJSONResult.ok(faceUrl);
     }
 }
