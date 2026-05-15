e3b6916615d9031b6524aec24e66b9d6728c0303
Blizzard <1315152965@qq.com>
2026-05-15 18:29:12 +0800

    fix: CommentController.delete()分别判断不同删评情况

--- helloChat-api/main-service-66/src/main/java/com/nlizzard/service/impl/CommentServiceImpl.java
+++ helloChat-api/main-service-66/src/main/java/com/nlizzard/service/impl/CommentServiceImpl.java
@@ -2,13 +2,19 @@ package com.nlizzard.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.nlizzard.base.BaseInfoProperties;
+import com.nlizzard.exceptions.GraceException;
+import com.nlizzard.grace.result.GraceJSONResult;
+import com.nlizzard.grace.result.ResponseStatusEnum;
 import com.nlizzard.mapper.CommentMapper;
+import com.nlizzard.mapper.FriendCircleMapper;
 import com.nlizzard.pojo.Comment;
+import com.nlizzard.pojo.FriendCircle;
 import com.nlizzard.pojo.Users;
 import com.nlizzard.pojo.bo.CommentBO;
 import com.nlizzard.pojo.vo.CommentVO;
 import com.nlizzard.service.CommentService;
 import com.nlizzard.service.UsersService;
+import com.nlizzard.utils.UserContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
@@ -18,6 +24,7 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -26,6 +33,8 @@ public class CommentServiceImpl extends BaseInfoProperties implements CommentSer
     private final CommentMapper commentMapper;
 
     private final UsersService usersService;
+
+    private final FriendCircleMapper friendCircleMapper;
 
     // 查询朋友圈的评论列表
     @Override
@@ -61,15 +70,40 @@ public class CommentServiceImpl extends BaseInfoProperties implements CommentSer
         return commentVO;
     }
 
-    // 删除朋友圈的评论
+    // 删除朋友圈的评论(1:删除自己的评论，2:删除别人对自己的朋友圈的评论)
     @Transactional
     @Override
     public void deleteComment(String commentUserId,String commentId,String friendCircleId) {
 
-        LambdaQueryWrapper<Comment> deleteWrapper = new LambdaQueryWrapper<>();
-        deleteWrapper.eq(Comment::getId, commentId)
+        String myId = UserContext.getUserId();
+        if(Objects.equals(myId,commentUserId)){
+            // 删除自己的评论
+            LambdaQueryWrapper<Comment> deleteWrapper = new LambdaQueryWrapper<>();
+            deleteWrapper.eq(Comment::getId, commentId)
                     .eq(Comment::getCommentUserId, commentUserId)
                     .eq(Comment::getFriendCircleId, friendCircleId);
+
+            commentMapper.delete(deleteWrapper);
+            return ;
+        }
+
+        // 删除别人对自己的朋友圈的评论
+        // 先判断是否是自己的朋友圈
+        LambdaQueryWrapper<FriendCircle> friendCircleQueryWrapper = new LambdaQueryWrapper<>();
+        friendCircleQueryWrapper.eq(FriendCircle::getId,friendCircleId)
+                                .eq(FriendCircle::getUserId,myId);
+        FriendCircle friendCircle = friendCircleMapper.selectOne(friendCircleQueryWrapper);
+
+        // 如果查询不到朋友圈，说明不是自己的朋友圈，没有权限删除评论
+        if(friendCircle == null){
+           GraceException.display(ResponseStatusEnum.NO_AUTH);
+        }
+
+        // 删除评论
+        LambdaQueryWrapper<Comment> deleteWrapper = new LambdaQueryWrapper<>();
+        deleteWrapper.eq(Comment::getId, commentId)
+                .eq(Comment::getCommentUserId, commentUserId)
+                .eq(Comment::getFriendCircleId, friendCircleId);
 
         commentMapper.delete(deleteWrapper);
     }
